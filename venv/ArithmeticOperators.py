353b0e368f3d6919e7c838a1fe8e84c4a4c7f5bb
sebi-vscode2 <stoklosasebastian144@proton.me>
2023-05-06 21:51:13 -0300

    Added ArithmeticOperators.py, ComparationOperators.py and OperatorPrecedence.py

---
+++ venv/ArithmeticOperators.py
@@ -0,0 +1,15 @@
+print(10 + 3)  # Addition
+print(10 - 3)  # Subtraction
+print(10 / 3)  # Floating point division
+print(10 // 3)  # Integer division
+print(10 % 3)  # Remainder of 10 / 3
+print(10 ** 3)  # 10 x 10 x 10 (exponent)
+
+x = 10
+x = x + 3  # 10 + 3 (13)
+x += 3  # 10 + 3 but shorter
+x -=  3 # Subtraction
+
+# And others!
+
+
---
+++ venv/OperatorPrecedence.py
@@ -0,0 +1,2 @@
+x = 10 + 3 * 2  # Python does: 10 + (3*2) which is 16
+x = (10 + 3) * 2  # Multiplies 13 * 2
