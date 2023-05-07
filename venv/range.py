493c7ad49b5a6d212a837024375164b2877da85a
sebi-vscode2 <stoklosasebastian144@proton.me>
2023-05-06 22:52:43 -0300

    Added ranges

---
+++ venv/range.py
@@ -0,0 +1,7 @@
+numbers = range(5)  # Value is the numbers 0 to 4
+numbers2 = range(5, 10)  # Value is the numbers  5 to 9
+for item in numbers:  # Prints each number of lists.
+    print(item)
+
+for number in range(5):
+    print(number)  # Prints from 0 to 4
