0db9cb12d617ec33964d22da57dd9dab8c9f6409
sebi-vscode2 <stoklosasebastian144@proton.me>
2023-05-06 22:01:01 -0300

    Finished Logical Operators and ComparisonOperators

---
+++ venv/ComparationOperators.py
@@ -0,0 +1,15 @@
+x = 3 > 2  # Returns true or false (Number comparison)
+# x = that means 3 is more than 2 (True)
+x = 3 == 2  # means 3 is equal to 2
+x = 3 != 2  # means 3 is not equal to 2
+
+# Signs:
+# > is more
+# >= is equal or more
+# < is less
+# <= is equal or less
+# == is equal to
+# != is not equal to
+
+
+print(x)
---
+++ venv/LogicalOperators.py
@@ -0,0 +1,15 @@
+price = 25
+print(price > 10)  # 25 is more than 10 (gives True)
+
+print(price > 10 and price < 30)  # 25 is more than 10 and less than 30
+
+price2 = 5
+print(price2 > 10 or price2 < 30)  # 5 has to be more than 30 or less than 30 to return true
+
+price3 = 5
+print(not price > 10)  # True if price3 is not 10
+
+# Types of Logical operators
+# and (Both)
+# or (at least one)
+# not (if it's not the same)
