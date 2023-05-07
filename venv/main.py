86a756a13bc167f7f51120fe5b90d30f6aa8a02d
sebi-vscode2 <stoklosasebastian144@proton.me>
2023-05-06 21:07:02 -0300

    Basic python variables and prints

---
+++ venv/main.py
@@ -0,0 +1,19 @@
+
+# Hello world
+
+print("Hello World")
+
+# Hello world with variable
+helloWorld = "Hello World"
+print(helloWorld)
+
+# Print with a variable and numbers
+
+# age = 20
+age = 30
+price = 19.95
+first_name = "Sebastian"
+is_online = True
+
+print(age)
+
