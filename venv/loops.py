43d3876441f7789a052587eff28351a630291dde
sebi-vscode2 <stoklosasebastian144@proton.me>
2023-05-06 22:46:33 -0300

    Renamed lists, added tuples to the name and finished loops

--- venv/loops.py
+++ venv/loops.py
@@ -4,3 +4,8 @@ while i <= 10:
     print(i * '*')
     i += 1
     # Prints the value of I while it's less than 5 (Change 5 for the times you want to run it)
+
+# For loops
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:  # For each number in the numbers list
+    print(item)  # Prints each item in separated lines
